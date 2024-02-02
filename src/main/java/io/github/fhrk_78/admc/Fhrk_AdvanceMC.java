2f4b159a5ebb4cbda669efd6267b79fb1452b196
PizzaHarumaki <78fhruby78@gmail.com>
2024-02-02 22:08:26 +0900

    きっぷ追加

--- src/main/java/io/github/fhrk_78/admc/Fhrk_AdvanceMC.java
+++ src/main/java/io/github/fhrk_78/admc/Fhrk_AdvanceMC.java
@@ -1,19 +1,26 @@
 package io.github.fhrk_78.admc;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.Item;
 
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Fhrk_AdvanceMC implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	// Make Item
+	public static final Item TICKET = new Item(new FabricItemSettings());
 
+	// Regist
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		Registry.register(Registries.ITEM, new Identifier("fhrk_advancemc", "ticket"), TICKET);
 	}
 }
