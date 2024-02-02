c7b5a802bf2e8d56ed730950bdeb4866d04e942a
PizzaHarumaki <78fhruby78@gmail.com>
2024-02-02 15:54:01 +0900

    first commit

---
+++ src/main/java/io/github/fhrk_78/admc/Fhrk_AdvanceMCDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.fhrk_78.admc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Fhrk_AdvanceMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
