44502e73054d1db92e11da7fd7f0e0a77e8a8faa
PizzaHarumaki <78fhruby78@gmail.com>
2024-02-03 00:33:07 +0900

    0.0.1

--- src/main/java/io/github/fhrk_78/admc/Fhrk_AdvanceMC.java
+++ src/main/java/io/github/fhrk_78/admc/Fhrk_AdvanceMC.java
@@ -2,25 +2,27 @@ package io.github.fhrk_78.admc;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Fhrk_AdvanceMC implements ModInitializer {
 	// Make Item
 	public static final Item TICKET = new Item(new FabricItemSettings());
+	//public static final Block TICKET_VENDING_MACHINE = new Block(FabricBlockSettings.create().strength(4.0f));
+	public static final Block TICKET_VENDING_MACHINE = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 
 	// Regist
 	@Override
 	public void onInitialize() {
 		Registry.register(Registries.ITEM, new Identifier("fhrk_advancemc", "ticket"), TICKET);
+		Registry.register(Registries.BLOCK, new Identifier("fhrk_advancemc", "ticket_vending_machine.json.json"), TICKET_VENDING_MACHINE);
+		Registry.register(Registries.ITEM, new Identifier("fhrk_advancemc", "ticket_vending_machine.json.json"), new BlockItem(TICKET_VENDING_MACHINE, new FabricItemSettings()));
 	}
 }
